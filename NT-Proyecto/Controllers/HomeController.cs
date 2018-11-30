40def997adc376474037811c96cb83e643eccfb9
David Santiago Gonzalez <dsgonzalez@practia.global>
2018-11-29 21:53:17 -0300

    agrego Url.Action para hacer vinculos desde la home view

--- NT-Proyecto/Controllers/HomeController.cs
+++ NT-Proyecto/Controllers/HomeController.cs
@@ -21,5 +21,10 @@ namespace NT_Proyecto.Controllers
             return View();
         }
 
+        public ActionResult Supermercado()
+        {
+            return View();
+        }
+
     }
 }
