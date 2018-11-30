db9fbe3a395d15229e23778a843b1fce2d39ed31
David Santiago Gonzalez <dsgonzalez@practia.global>
2018-11-30 14:11:11 -0300

    Json de Barrios y puesta a punto del JavaScript

---
+++ NT-Proyecto/Scripts/JavaScript.js
@@ -0,0 +1,58 @@
+﻿$(document).ready(function () {
+
+    var quantitiy = 0;
+    $('.qty-plus').click(function (e) {
+
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#qty').val());
+
+        // If is not undefined
+
+        $('#qty').val(quantity + 1);
+
+
+        // Increment
+
+    });
+
+    $('.qty-minus').click(function (e) {
+        // Stop acting like a button
+        e.preventDefault();
+        // Get the field name
+        var quantity = parseInt($('#qty').val());
+
+        // If is not undefined
+
+        // Increment
+        if (quantity > 1) {
+            $('#qty').val(quantity - 1);
+        }
+    });
+
+});
+
+
+
+$("#login").click(function () {
+    $("#myModal").modal();
+});
+
+$(document).ready(function () {
+
+    let dropdown = $("#barrio");
+    dropdown.empty();
+
+    dropdown.append('<option selected="true" disabled>Seleccione su barrio</option>');
+    dropdown.prop('selectedIndex', 0);
+
+    const url = 'https://api.myjson.com/bins/rrlzm';
+
+
+    $.getJSON(url, function (data) {
+        $.each(data, function (key, entry) {
+            dropdown.append($('<option></option>').attr('value', entry.abbreviation).text(entry.name));
+        })
+    });
+});
